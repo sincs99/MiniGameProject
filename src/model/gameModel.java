cd382350735406f8df747829bb68c7820f8900df
pascal <pascal@192.168.0.178>
2020-03-23 14:54:13 +0100

    Test Commit new Branch

--- src/model/gameModel.java
+++ src/model/gameModel.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
@@ -17,31 +18,30 @@ public class gameModel {
 
 	protected static int rowSize = 6;
 	protected static int colSize = 7;
-	
+
 	protected static Color GAME_BOARD = Color.BLUEVIOLET;
 
 	// Color for Row Hover effect
 	protected final static Color HOVER_FILL = new Color(0.5, 1.0, 1.0, 0.3);
 
 	public static Shape buildBoard() {
-		
-		
 
 		Shape form = new Rectangle((colSize + 1) * view.gameGrid.DISC_SIZE, (rowSize + 1) * view.gameGrid.DISC_SIZE);
-		
 
 		for (int yAxle = 0; yAxle < rowSize; yAxle++) {
 			for (int xAxle = 0; xAxle < colSize; xAxle++) {
 
 				Circle c = new Circle(view.gameGrid.DISC_SIZE / 2);
+
 				c.setCenterX(view.gameGrid.DISC_SIZE / 2);
 				c.setCenterY(view.gameGrid.DISC_SIZE / 2);
 				c.setTranslateX(xAxle * (view.gameGrid.DISC_SIZE + 5) + view.gameGrid.DISC_SIZE / 4);
 				c.setTranslateY(yAxle * (view.gameGrid.DISC_SIZE + 5) + view.gameGrid.DISC_SIZE / 4);
 
 				form = Shape.subtract(form, c);
-				
+
 				form.setFill(GAME_BOARD);
+
 			}
 		}
 
@@ -55,7 +55,7 @@ public class gameModel {
 
 		for (int xCol = 0; xCol < colSize; xCol++) {
 
-			Rectangle r1 = new Rectangle(view.gameGrid.DISC_SIZE, (rowSize + 1) * view.gameGrid.DISC_SIZE);
+			Rectangle r1 = new Rectangle(view.gameGrid.DISC_SIZE, (rowSize +1) * view.gameGrid.DISC_SIZE);
 			r1.setTranslateX(xCol * (view.gameGrid.DISC_SIZE + 5) + view.gameGrid.DISC_SIZE / 4);
 
 			r1.setFill(Color.TRANSPARENT);
@@ -94,11 +94,9 @@ public class gameModel {
 	public static Color getGameBoard() {
 		return GAME_BOARD;
 	}
-	
+
 	public static void setGameBoard(Color GAME_BOARD) {
 		gameModel.GAME_BOARD = GAME_BOARD;
 	}
-	
-	
 
 }
